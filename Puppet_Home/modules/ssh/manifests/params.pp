070f5ca977480bf6974b20bb9f81a23e458465cf
suryajayanthi <murthy.jayanthi@gmail.com>
2015-05-20 15:02:36 -0400

    ssh enhancements

--- Puppet_Home/modules/ssh/manifests/params.pp
+++ Puppet_Home/modules/ssh/manifests/params.pp
@@ -3,10 +3,17 @@ class ssh::params {
     'Debian': {
       $sshd_package = 'ssh'
       $sshd_service = 'ssh'
+      $sshd_service_config = '/etc/ssh/sshd_config'
     }
     'Redhat': {
       $sshd_package = 'openssh-server'
       $sshd_service = 'sshd'
+      $sshd_service_config = '/etc/ssh/sshd_config'
+    }
+    'Solaris': {
+      $sshd_package = 'SUNWssh'
+      $sshd_service = 'ssh'
+      $ssh_service_config = '/etc/ssh/sshd_config'
     }
     default: {
       fail("Login class does not work on osfamily : ${::osfamily}")
