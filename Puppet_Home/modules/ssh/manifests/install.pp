7f645f330a20a4e0a408d83c7ceba1a8076359cc
suryajayanthi <murthy.jayanthi@gmail.com>
2015-05-20 09:39:18 -0400

    default => fail()

--- Puppet_Home/modules/ssh/manifests/install.pp
+++ Puppet_Home/modules/ssh/manifests/install.pp
@@ -4,7 +4,8 @@ class ssh::install {
     'RedHat'  =>  "openssh-server",
     'Debian'  =>  "openssh-server",
     'Solaris' =>  "openssh",
-    default   =>  "ssh",
+    # default   =>  "ssh",
+    default   =>  fail("Module ssh does not support osfamily: ${::osfamily}")
   }
   package {'ssh':
     ensure  =>  present,
